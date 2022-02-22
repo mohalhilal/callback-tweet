1a1e7fe5b5c80dd0f863eb736f239cec31d8bb81
Mohammad AlHilal <53252937+mohalhilal@users.noreply.github.com>
2022-02-22 22:05:51 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+
+if(!empty($_GET['oauth_verifier'])){
+        echo "OAUTH VERIFIER : ".$_GET['oauth_verifier'];
+        echo "OAUTH TOKEN  : ".$_GET['oauth_token'];
+    } else {
+        echo "CUMA PELARIAN AJA"
+    }
+
+
+
+
+?>
